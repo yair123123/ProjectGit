9463b25129224f79273b785fb85dad4bdef79c8f
yair123123 <ytykhn8@gmail.com>
2024-09-01 12:17:35 +0300

    Create Model Student

--- ProjectGit/Models/Students.cs
+++ ProjectGit/Models/Students.cs
@@ -1,6 +1,11 @@
-﻿namespace ProjectGit.Models
+﻿
+namespace ProjectGit.Models
 {
     public class Students
     {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int NumClass { get; set; }
+        public int gradePointAverage { get; set; }
     }
 }
