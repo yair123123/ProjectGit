a572df52f3c3f9226b024994e55ba0b657df6429
a8812388123 <bb8812388@gmail.com>
2024-09-01 15:27:43 +0300

    added a service folder

---
+++ ProjectGit/Services/CalculatorServices.cs
@@ -0,0 +1,9 @@
+﻿namespace ProjectGit.Services
+{
+    public class CalculatorServices
+    {
+        public static int Plus(int x, int y) { return x+y; }
+
+        public static int Minus(int x, int y) { return x - y;}
+    }
+}
